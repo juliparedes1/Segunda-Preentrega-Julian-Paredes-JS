2cff2d7a0c67a9c4478204fabf282f693a25160f
juliCoder <juliparedes451@gmail.com>
2024-03-02 19:09:16 -0300

    agregado de validaciones y explicacion del codigo

--- js/main.js
+++ js/main.js
@@ -16,10 +16,10 @@ class Compra {
     constructor(producto, cantidad) {
         this.producto = producto;
         this.cantidad = cantidad;
-        
+
     }
-    valorDeCompra(){
-        return this.producto.precio * this.cantidad 
+    valorDeCompra() {
+        return this.producto.precio * this.cantidad
     }
 }
 //creacion del array contenedor de objetos producto,
@@ -42,9 +42,9 @@ let banderaCompras = false;
 //la bandera compras indica si la persona termino de elegir todas las cosas que queria comprar
 
 while (!banderaCompras) {
-    
+
     let ingreso = prompt("Ingrese el producto que desea, si desea ver el nombre de los productos presione 0");
-//si el ingreso no contiene nada entonces pide que ingrese correctamente el nombre de un elemento
+    //si el ingreso no contiene nada entonces pide que ingrese correctamente el nombre de un elemento
     while (ingreso == "" || ingreso == "0") {
         if (ingreso == "0") {
             const rta = [], valores = [];
@@ -56,49 +56,61 @@ while (!banderaCompras) {
                 mostrarMixto.push(rta[i]);
                 mostrarMixto.push(valores[i]);
             }
+            //aca si presiono el cero, muestra los elementos y sus valores unitarios
             alert(mostrarMixto.join("\n"));
         }
         ingreso = prompt("Ingrese un nombre valido, si desea ver el nombre de los productos presione 0");
-
     }
-
+    // tuve que usar un let para poder modificar el codigo de abajo
     let productosEncontrados = arrProductos.filter((el) => {
         return el.nombre.includes(ingreso);
     });
-    
-
-    
-
+    // valida que si no se encontro ningun elemento dentro del array se escriba nuevamente otro nombre, por si se equivoco en el tipeo
+    if (productosEncontrados.length == 0) {
+        ingreso = prompt("Ingrese un nombre valido, no se encontraron productos con ese nombre");
+        productosEncontrados = arrProductos.filter((el) => {
+            return el.nombre.includes(ingreso);
+        });
+        
+    }
+    // muestra cuales son los elementos que coinciden con la busqueda y su cantidad en stock
     productosEncontrados.forEach((iterator, i = 0) => {
         alert(`Ustes ha encontrado: ${iterator.nombre} \n en stock: ${iterator.cantidadEnStock} \n digite  ${i + 1} para seleccionarlo`);
         i++;
     });
-    let indice = prompt("Digite el numero el del objeto ");
-    // no se como validar que si por ejemplo escribo soga, y me salen los 3 tipos de soga que tengo, como hago para que el indice tenga que estar entre esos 3, y si se equivoca y pone 4 le pida que lo reescriba
+
+    //verifica que tanto el indice seleccionado sea logico y que la cantidad no supere el stock
+    let indice = parseInt(prompt("Digite el numero el del objeto "));
+    while(!(indice > 0 && indice <= productosEncontrados.length)) {
+    indice = prompt("Usted ha ingresado un numero erroneo, pruebe nuevamente");
+    }; 
     let cantidad = prompt("Digite la cantidad que quiere comprar")
     while (cantidad > productosEncontrados[(indice - 1)].cantidadEnStock) {
         alert("La cantidad que desea comprar es mayor al stock lo sentimos")
         cantidad = prompt("Digite la cantidad que quiere comprar")
+    };
 
-    }
+    //aca una vez seleccionado el producto, reduce su cantidad en stock para las siguientres compras, creo que en verdad deberia usar el metodo foreach, pero como funciona no lo toco jaja
     arrProductos.find((el) => {
         if (el.nombre == productosEncontrados[(indice - 1)].nombre) {
             el.cantidadEnStock = el.reduccionDeStock(cantidad);
         }
     });
+
+    //aca crea las ordenes de compra
     ordenesDeCompra.push(new Compra(productosEncontrados[(indice - 1)], cantidad));
     ingreso = prompt("Si quiere realizar otra compra presione aceptar, si desea terminar presione cancelar");
     if (ingreso == null) {
         banderaCompras = true;
     }
 }
-
-const totalDeCompras = ordenesDeCompra.map((el)=>{
+// esta aprte del codigo crea un array con el valor de cada una de las ordenes de compra
+const totalDeCompras = ordenesDeCompra.map((el) => {
     return el.valorDeCompra();
 });
-
+//aca se suman todos los valores para dar el precio total;
 const precioTotal = totalDeCompras.reduce((acc, actual) => {
-return acc+actual
+    return acc + actual
 }, 0)
 
 alert(`el precio total es de ${precioTotal}`);
